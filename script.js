85f5efa5f6e4f94d6056d9d45430dd4d3a57849c
nelsonjm <njoeden05@gmail.com>
2021-10-22 09:48:48 -0700

    im done

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("hello i am alive");
+
+var name = "joeden"
+
+var numb = 2
+
+var numb2 = 3
+console.log(name);
+console.log(numb + numb2);
+
+function addition(numb1, numb2){
+    console.log(numb1 + numb2);
+}
+addition (10, 31)
+
+function introduction(name, age){
+    console.log('hello my name is ' + name + ' and i am ' + age + ' years old ')
+}
+introduction("joeden", "16");
+
+document.write ('<h1> This is my javascript project</h1>') 
+
+alert('this was all made in js')
+
+prompt('do you like this class')
+
+
+
+
